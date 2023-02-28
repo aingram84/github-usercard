1f27e6b386b0361da588765d49962799be171e89
Adam Ingram <kr0nai84@gmail.com>
2023-02-27 22:12:41 -0800

    made github user cards

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,3 +1,5 @@
+const { default: axios } = require("axios");
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
@@ -16,7 +18,60 @@
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+function gitCard(ghUserName) {
+  const url = `https://api.github.com/users/${ghUserName}`;
+  axios.get(url).then(response => {
+    document.querySelector(".cards").appendChild(githubUserCard(response.data))
 
+  })
+    .catch(error => {
+      console.error(error);
+    })
+    .finally(() => console.log("Test"));
+}
+
+function githubUserCard(gitObject) {
+  const cardWrapper = document.createElement("div");
+  const image = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const name = document.createElement("h3");
+  const userName = document.createElement("p");
+  const location = document.createElement("p");
+  const proDesc = document.createElement("p");
+  const address = document.createElement("a");
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const bio = document.createElement("p");
+
+  name.classList.add("name");
+  userName.classList.add("username");
+  cardInfo.classList.add("card-info");
+  cardWrapper.classList.add("card");
+
+  image.src = gitObject.avatar_url;
+  name.textContent = gitObject.name;
+  userName.textContent = gitObject.login;
+  location.textContent = gitObject.location;
+  proDesc.textContent = gitObject.blog;
+  address.textContent = gitObject.html_url;
+  address.href = gitObject.html_url;
+  followers.textContent = `Followers: ${gitObject.followers}`;
+  following.textContent = `Following: ${gitObject.following}`;
+  bio.textContent = `Bio: ${gitObject.bio}`;
+
+  cardWrapper.appendChild(image);
+  cardWrapper.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(address);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(proDesc);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  return cardWrapper;
+}
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -28,7 +83,11 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ["aingram84", "evoingram", "bigknell", "justsml", "luishrd", "tetondan"];
+
+for (let i = 0; i < followersArray.length; i++) {
+  gitCard(followersArray[i]);
+}
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
